42042b7f2c925ba6c61585f79a3cbb7f36698764
Robert Vokac <robertvokac@robertvokac.com>
2026-06-13 11:02:58 +0200

    fix(Media/Playlist): add CNAHelper, NOXNA GetTypeName, GetHashCode, private ctor

--- include/Microsoft/Xna/Framework/Media/Playlist.hpp
+++ include/Microsoft/Xna/Framework/Media/Playlist.hpp
@@ -3,6 +3,7 @@
 
 #include <string>
 
+#include "CNA/CNAHelper.hpp"
 #include "System/IDisposable.hpp"
 #include "System/Object.hpp"
 #include "System/TimeSpan.hpp"
@@ -59,6 +60,12 @@ namespace Microsoft::Xna::Framework::Media
         [[nodiscard]] bool Equals(const Playlist* other) const;
 
         /**
+         * @brief Gets the hash code for this Playlist instance.
+         * @return Hash code of the object.
+         */
+        [[nodiscard]] int GetHashCode() const;
+
+        /**
          * @brief Returns a string representation of this playlist.
          *
          * @return Playlist name string.
@@ -66,12 +73,15 @@ namespace Microsoft::Xna::Framework::Media
         [[nodiscard]] std::string ToString() const;
 
         /** @brief Returns the fully-qualified .NET type name. */
-        [[nodiscard]] const std::string& GetTypeName() const override;
+        NOXNA [[nodiscard]] const std::string& GetTypeName() const override;
 
         /** @brief Returns whether two playlists are equal. */
         friend bool operator==(const Playlist& lhs, const Playlist& rhs);
 
         /** @brief Returns whether two playlists are not equal. */
         friend bool operator!=(const Playlist& lhs, const Playlist& rhs);
+
+    private:
+        Playlist();
     };
 }
