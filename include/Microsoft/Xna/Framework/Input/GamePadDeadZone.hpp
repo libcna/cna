21dbf2c7cd1fd83c63dfd954b3a0b5d15d956bbb
Robert Vokac <robertvokac@robertvokac.com>
2026-06-13 10:01:22 +0200

    fix(GamePadDeadZone): expand enum value Doxygen from FNA param descriptions

--- include/Microsoft/Xna/Framework/Input/GamePadDeadZone.hpp
+++ include/Microsoft/Xna/Framework/Input/GamePadDeadZone.hpp
@@ -4,15 +4,20 @@
 namespace Microsoft::Xna::Framework::Input
 {
     /**
-     * @brief Specifies a type of dead zone processing to apply to analog sticks when calling GetState.
+     * @brief Specifies a type of dead zone processing to apply to the controllers analog sticks
+     *        when calling GetState.
      */
     enum class GamePadDeadZone
     {
-        /** @brief Values are returned as raw values; no dead zone processing is applied. */
+        /** @brief The values of each stick are not processed and are returned by GetState as raw values.
+         *         Use this if you intend to implement your own dead zone processing. */
         None,
-        /** @brief The X and Y positions of each stick are compared against the dead zone independently. */
+        /** @brief The X and Y positions of each stick are compared against the dead zone independently.
+         *         This is the default when calling GetState. */
         IndependentAxes,
-        /** @brief The combined X and Y position of each stick is compared to the dead zone. */
+        /** @brief The combined X and Y position of each stick is compared to the dead zone.
+         *         Provides better control than IndependentAxes when the stick is used as a
+         *         two-dimensional control surface, such as in a first-person game. */
         Circular,
     };
 }
