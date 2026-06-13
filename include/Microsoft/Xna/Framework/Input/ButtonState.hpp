67f0e33c41ca3492fde6c10777569b71d848afbc
Robert Vokac <robertvokac@robertvokac.com>
2026-06-13 09:45:48 +0200

    fix(ButtonState): remove stale created-by comment, align @brief with FNA

--- include/Microsoft/Xna/Framework/Input/ButtonState.hpp
+++ include/Microsoft/Xna/Framework/Input/ButtonState.hpp
@@ -1,14 +1,10 @@
 // SPDX-License-Identifier: MS-PL
-//
-// Created by robertvokac on 5/25/25.
-//
-
 #pragma once
 
 namespace Microsoft::Xna::Framework::Input
 {
     /**
-     * @brief Specifies the state of a button on a mouse or gamepad.
+     * @brief Defines a button state for buttons of mouse, gamepad or joystick.
      */
     enum class ButtonState
     {
