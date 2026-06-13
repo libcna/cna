e87a2f84aa0066e67e997c5aa5524d8495c84941
Robert Vokac <robertvokac@robertvokac.com>
2026-06-13 11:00:38 +0200

    fix(Media/PictureCollection): add CNAHelper, NOXNA on iterators+GetTypeName, private ctor

--- include/Microsoft/Xna/Framework/Media/PictureCollection.hpp
+++ include/Microsoft/Xna/Framework/Media/PictureCollection.hpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 
+#include "CNA/CNAHelper.hpp"
 #include "System/IDisposable.hpp"
 #include "System/Object.hpp"
 #include "SharpRuntime/SharpRuntimeHelper.hpp"
@@ -49,21 +50,23 @@ namespace Microsoft::Xna::Framework::Media
         [[nodiscard]] Picture* operator[](SharpRuntime::intcs index) const;
 
         /** @brief Returns an iterator to the first picture. */
-        [[nodiscard]] iterator begin();
+        NOXNA [[nodiscard]] iterator begin();
 
         /** @brief Returns an iterator past the last picture. */
-        [[nodiscard]] iterator end();
+        NOXNA [[nodiscard]] iterator end();
 
         /** @brief Returns a const iterator to the first picture. */
-        [[nodiscard]] const_iterator begin() const;
+        NOXNA [[nodiscard]] const_iterator begin() const;
 
         /** @brief Returns a const iterator past the last picture. */
-        [[nodiscard]] const_iterator end() const;
+        NOXNA [[nodiscard]] const_iterator end() const;
 
         /** @brief Returns the fully-qualified .NET type name. */
-        [[nodiscard]] const std::string& GetTypeName() const override;
+        NOXNA [[nodiscard]] const std::string& GetTypeName() const override;
 
     private:
+        PictureCollection();
+
         std::vector<Picture*> innerList_;
     };
 }
